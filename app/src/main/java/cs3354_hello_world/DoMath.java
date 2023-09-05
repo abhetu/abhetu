8a69e50f2a7ca98256eb3b584c55f73deb23160c
Ted Lehr <ted.lehr@txstate.edu>
2023-09-05 11:51:55 -0500

    Initial Commit

---
+++ app/src/main/java/cs3354_hello_world/DoMath.java
@@ -0,0 +1,29 @@
+package cs3354_hello_world;
+
+public class DoMath {
+    protected int x;
+    
+    public int increment(int number) 
+    {
+        x = number++; 
+        return x;
+    }
+      
+    public int decrement(int number) 
+    {
+        x = number--;
+        return x;
+    }
+   
+    public void printClassAndPackageName() {
+        // The explicit declaration of ThisClass is unnecessary but hopefull useful 
+        // for illustrating what is going on to students.
+        Class<?> ThisClass = this.getClass();
+
+        System.out.println("Class name: " + ThisClass.getName());
+        System.out.println("Instance name: " + this);
+        System.out.println("Package name: " + ThisClass.getPackageName());
+    }
+
+
+}
