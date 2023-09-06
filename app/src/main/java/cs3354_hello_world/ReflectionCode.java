9f2750a9b3d9b273deb6d3a9a09cd9e4852581f7
Ted Lehr <ted.lehr@txstate.edu>
2023-09-06 13:44:10 -0500

    change gitignore added parameter and type to reflection

--- app/src/main/java/cs3354_hello_world/ReflectionCode.java
+++ app/src/main/java/cs3354_hello_world/ReflectionCode.java
@@ -1,6 +1,7 @@
 package cs3354_hello_world;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public abstract class ReflectionCode {
 
@@ -16,17 +17,33 @@ public abstract class ReflectionCode {
 
     public void printFields() {
         Field[] fields = this.getClass().getDeclaredFields();
+        // Loop through the fields and print out their names and types
         for (Field field : fields) {
-            System.out.println("Field name: " + field.getName());
+            String fieldName = field.getName();
+            Class<?> fieldType = field.getType(); 
+
+            System.out.println("Field Name: " + fieldName);
+            System.out.println("Field Type: " + fieldType.getName());
+            System.out.println();
         }
     }
 
     public void printMethods() {
         Method[] methods = this.getClass().getDeclaredMethods();
 
-        // Loop through the methods and print their names
+        // Loop through the methods and print their names and types
         for (Method method : methods) {
-            System.out.println("Method name: " + method.getName());
+            Class<?> returnType = method.getReturnType();
+
+            System.out.println("Method Name: " + method.getName());
+            System.out.println("Return Type: " + returnType.getName());
+             // Get parameters
+             Parameter[] parameters = method.getParameters();
+             System.out.println("Parameters:");
+             for (Parameter parameter : parameters) {
+                 System.out.println("  " + parameter.getType().getName() + " " + parameter.getName());
+             }
+            System.out.println();
         } 
     }
 
